cb2bab1dc76e5dfe2f0e289f036d217e972a8b01
이창한 <changhan.dev@gmail.com>
2024-06-15 17:33:20 +0900

    First Commit

---
+++ src/main/java/com/example/pastsnap/PastSnapApplication.java
@@ -0,0 +1,14 @@
+package com.example.pastsnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastSnapApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/example/pastsnap/PastSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pastsnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastSnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
